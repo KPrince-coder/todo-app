78926c11956774494f5cd78715875502ebf0df81
Prince Kyeremeh <kyeremehprince68@gmail.com>
2023-12-02 11:56:14 +0000

    Include alt text to logo image

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,7 +5,11 @@ const Logo = () => {
 		<div className="">
 			<p className="prose font-montserrat w-full text-3xl font-bold text-violet-600">
 				<span className="text-stone-900">Task</span>Ninja
-				<img src={Logo_Icon} className="my-0 -ml-1 inline-block h-12" />
+				<img
+					src={Logo_Icon}
+					alt="logo ninja icon"
+					className="my-0 -ml-1 inline-block h-12"
+				/>
 			</p>
 		</div>
 	);
